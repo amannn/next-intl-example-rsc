9cef2b191a75dd3ce6e03d91474df821697ac0c7
Jan Amann <jan@amann.work>
2023-01-10 12:54:30 +0100

    Revert "Add back static params"
    This reverts commit 4b02f64392bcfbe07769a054a26550d5118cb909.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,14 +1,9 @@
 import {useLocale} from 'next-intl';
 import {ReactNode} from 'react';
-import i18n from '../../i18n';
 
 type Props = {
   children: ReactNode;
 };
-
-export function generateStaticParams() {
-  return i18n.locales.map((locale) => ({locale}));
-}
 
 export default async function LocaleLayout({children}: Props) {
   const locale = useLocale();
