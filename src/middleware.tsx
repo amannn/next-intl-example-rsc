c88e07563120c5900826533b9b74a910d86ba6b6
Jan Amann <jan@amann.work>
2023-02-08 18:02:14 +0100

    Add host logging

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,9 +1,20 @@
 import {createIntlMiddleware} from 'next-intl/server';
+import {NextRequest} from 'next/server';
 
-export default createIntlMiddleware({
+const middleware = createIntlMiddleware({
   locales: ['en', 'de'],
   defaultLocale: 'en'
 });
+
+export default function handler(req: NextRequest) {
+  const value = req.headers.get('host') || 'N/A';
+
+  const response = middleware(req);
+
+  response.cookies.set('test', value);
+
+  return response;
+}
 
 export const config = {
   matcher: ['/((?!_next).*)']
