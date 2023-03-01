ff5eabd6cabd13131ab8d2481ffe51a62641577a
Jan Amann <jan@amann.me>
2023-03-01 16:57:54 +0100

    Improve caching

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
-const ms = require('ms');
 const withNextIntl = require('next-intl/plugin')();
 
 module.exports = withNextIntl({
-  experimental: {appDir: true},
-  headers() {
-    return ['/', '/de', '/en'].map((path) => ({
-      source: path,
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: [
-            `s-maxage=${ms('1d') / 1000}`,
-            `stale-while-revalidate=${ms('1y') / 1000}`
-          ].join(', ')
-        }
-      ]
-    }));
-  }
+  experimental: {appDir: true}
 });
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,9 +1,25 @@
+import ms from 'ms';
 import createIntlMiddleware from 'next-intl/middleware';
+import {NextRequest} from 'next/server';
 
-export default createIntlMiddleware({
+const middleware = createIntlMiddleware({
   locales: ['en', 'de'],
   defaultLocale: 'en'
 });
+
+export default function handler(req: NextRequest) {
+  const response = middleware(req);
+
+  response.headers.set(
+    'Cache-Control',
+    [
+      `s-maxage=` + ms('1d') / 1000,
+      `stale-while-revalidate=` + ms('1y') / 1000
+    ].join(', ')
+  );
+
+  return response;
+}
 
 export const config = {
   // Skip all non-content paths
